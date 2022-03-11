e5feb67b442b35d59b3d015ea164326b2480693c
fiRinhozor <bulatgilfanov0@gmail.com>
2022-03-10 17:12:57 -0800

    everything working, next commit update getResult() with switch statement

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
-const result = document.getElementById("result");
+const resultDisplay = document.getElementById("result");
 const possibleChoice = document.querySelectorAll("button");
 let userChoice;
 let computerChoice;
+let result;
 
 possibleChoice.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
   })
 );
 
@@ -25,4 +27,30 @@ function generateComputerChoice() {
     computerChoice = "paper";
   }
   computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "it's a draw";
+  }
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "you win!";
+  }
+  if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "you lost!";
+  }
+  if ((computerChoice = "paper" && userChoice === "scissors")) {
+    result = "you win!";
+  }
+  if ((computerChoice = "paper" && userChoice === "rock")) {
+    result = "you lost!";
+  }
+  if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "you win!";
+  }
+  if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "you lost";
+  }
+
+  resultDisplay.innerHTML = result;
 }
