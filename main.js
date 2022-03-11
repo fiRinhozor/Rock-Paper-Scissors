41b887bc351536e9ffee56fc93623fce5a1142eb
fiRinhozor <bulatgilfanov0@gmail.com>
2022-03-10 16:51:01 -0800

    pushin first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const result = document.getElementById("result");
+const possibleChoice = document.querySelectorAll("button");
+let userChoice;
+let computerChoice;
+
+possibleChoice.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+  })
+);
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "scissors";
+  }
+  if (randomNumber === 3) {
+    computerChoice = "paper";
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
